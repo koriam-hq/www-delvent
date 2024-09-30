a88e49485a058c2c242350f62699a915f728d054
Josh <mail@chukwuanu.com>
2024-09-30 19:06:09 +0100

    feat: add notion storage (#5)

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const notificationCard = document.getElementById("notificationCard");
 
   if (form) {
-    form.addEventListener("submit", function (event) {
+    form.addEventListener("submit", async function (event) {
       event.preventDefault();
 
       const firstName = document.getElementById("firstName").value;
       const lastName = document.getElementById("lastName").value;
       const email = document.getElementById("email").value;
 
-      console.log("Form submitted:", { firstName, lastName, email });
+      console.debug("Form submitted:", { firstName, lastName, email });
 
       if (formContainer) {
         formContainer.style.display = "none";
@@ -21,6 +21,27 @@ document.addEventListener("DOMContentLoaded", function () {
       if (notificationCard) {
         notificationCard.style.display = "block";
       }
+
+      const data = new FormData();
+      data.append("first_name", firstName);
+      data.append("last_name", lastName);
+      data.append("email", email);
+      console.debug({ data });
+
+      const response = await fetch("https://delvent.io/api/wait-list", {
+        mode: "no-cors",
+        // mode: "cors",
+        headers: {
+          credentials: "include",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        },
+        method: "POST",
+        body: data,
+      });
+      console.debug({ response: await response.json() });
 
       form.reset();
     });
